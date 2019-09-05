f6167d5932113a916fe77969cce42ef0a5913c54
nquist <47543479+nquist@users.noreply.github.com>
2019-09-05 12:29:30 -0700

    Add files via upload

---
+++ hw1/hw1_prob3.R
@@ -0,0 +1,9 @@
+# Grade: 2.5/5
+# Excellent!
+
+rm(list = ls())
+
+print("Hello world!")
+
+samp <- rnorm(100, mean = 10, sd = 3)
+print(mean(samp))
---
+++ hw1/hw1_prob3_try2.R
@@ -0,0 +1,10 @@
+# Regrade: 5/5
+# Excellent!
+
+print("This is an edit to the code for a second try.")
+rm(list = ls())
+
+print("Hello world!")
+
+samp <- rnorm(100, mean = 10, sd = 3)
+print(mean(samp))
